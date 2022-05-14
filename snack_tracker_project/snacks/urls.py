fb5c5de7e6281b9d7ae48dcb3944aa61a81a7985
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-05-14 13:25:01 +0300

    last draft

---
+++ snack_tracker_project/snacks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import SnackListView, SnackDetailView
+
+urlpatterns = [
+    path('snacks-list', SnackListView.as_view(), name='snacks_list'),
+    path('detail-view/<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
+]
